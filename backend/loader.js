96963ac8ad6865ae81ae8ee48ae7be28a21aaac7
claytonrfsap <clayton.fernandes@outlook.com.br>
2018-02-28 08:22:57 -0300

    Initial commit

--- backend/loader.js
+++ backend/loader.js
@@ -1,1 +1,21 @@
 const port = 3003
+
+// midleware parser do body da requisição, pega dados dos formulario
+const bodyParser = require('body-parser');
+const express = require('express');
+const server = express()
+
+//urlenconded é o formato dos dados do forumario, ou seja faz o tratamento dos dados na submissao
+// USE - toda requisiçao vai passar por esse middleware
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+//Se fez o biding direitinho
+server.listen(port, function () {
+  console.log(`BACKEND  is running on port ${port}.`);
+})
+
+
+server.use(function (req, res, next) {
+  res.send('funcionou')
+})
