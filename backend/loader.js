1c06d14bcb4891be7d071565609e3667c2e3b6bf
claytonrfsap <clayton.fernandes@outlook.com.br>
2018-02-28 08:33:23 -0300

    arquivo_loader_correto
    continuar

--- backend/loader.js
+++ backend/loader.js
@@ -14,14 +14,3 @@ server.use(bodyParser.json())
 server.listen(port, function () {
   console.log(`BACKEND  is running on port ${port}.`);
 })
-
-
-server.use(function (req, res, next) {
-  console.log('meu middleware 1');
-  next()
-})
-
-server.use(function (req, res, next) {
-  console.log('meu middleware 2');
-  res.send('funcionou novamente')
-})
